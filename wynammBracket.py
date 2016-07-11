3545db12b4ed2491d170f3ea590f3864bf75ad11
ammgws <zilla.icles@gmail.com>
2016-07-11 21:35:52 +0900

    have a look

--- wynammBracket.py
+++ wynammBracket.py
@@ -7,26 +7,50 @@
 5) repeat'''
 
 import random
+import sys
 
-opponentsDict = {}
-scoresDict = {}
+# Get absolute path of the dir script is run from
+cwd = sys.path[0]
 
-possibleTeams = ['FC Bayern', 'FC Barcelona', 'Real Madrid', 'PSG', 'Chelsea', 'Man City', 'Arsenal', 'Juventus',
-                 'Bor Dortmund', 'Atletico Madrid', 'Man Utd.', 'Valencia', 'Napoli', 'Spurs', 'Liverpool', 'Roma', 'Sevilla FC',
-                 'Villarreal CF', 'Bayer 04', 'Vfl Wolfsburg', 'Inter', 'Athletic Bilbao', 'SL Benfica', 'Sporting CP', 'Zenit',
-                 'FC Schalke 04', 'Milan', 'Everton', 'Lazio', 'FC Porto', "Bor. M'gladbach", 'Besiktas', 'Fenerbahce' 'Fiorentina',
-                 'Olym Lyonnais', 'AS Monaco', 'Real Sociedad', 'Newcastle Utd.', 'Stoke', 'West Ham', 'Celta Vigo', 'Swansea', 
-                 'Olym. Marseille', 'Torino', 'Real Betis', 'Leicester City', 'Southampton', 'Malaga CF', 'Crystal Palace', 'PSV',
-                 '1899 Hoffenheim', 'Shakhtar Donetsk', 'Ajax', 'AS Saint-Etienne', 'CSKA Moscow', 'RC Deportivo', 'Sunderland',
-                 'Watford', 'Stade Rennais', 'RCD Espanyol']
-           
-shuffledTeams = []
-for item in possibleTeams:
-    shuffledTeams.append(item)
-random.shuffle(shuffledTeams)
+opponents_dict = {}
+scores_dict = {}
 
+with open(cwd + '/wynnammBracket_possible_teams.txt', 'r') as file:
+    #possible_teams=file.readlines()
+    possible_teams=file.read().splitlines()
+random.shuffle(possible_teams) # Randomise teams
+
+if len(possible_teams) < 32:
+    raise Exception("Minimum of 32 teams required in text file")
+
+# Create random list of 16 teams each
+possible_wyn_teams = possible_teams[:16]
+possible_amm_teams = possible_teams[16:32]
+
+# Present 2 teams at a time and have each choose their 8 teams
+chosen_wyn_teams = []
+chosen_amm_teams = []
+print('***Choosing Wyn teams***')
+for i, team in enumerate(possible_wyn_teams):
+    if i % 2 == 0:
+        selection = input("Wyn - Enter 1 for {0} or 2 for {1}: ".format(possible_wyn_teams[i], possible_wyn_teams[i+1]))
+        if selection == '1':
+            chosen_wyn_teams.append(possible_wyn_teams[i])
+        elif selection == '2':
+            chosen_wyn_teams.append(possible_wyn_teams[i+1])
+print('***Choosing Amm teams***')
+for i, team in enumerate(possible_amm_teams):
+    if i % 2 == 0:
+        selection = input("Amm - Enter 1 for {0} or 2 for {1}: ".format(possible_amm_teams[i], possible_amm_teams[i+1]))
+        if selection == '1':
+            chosen_amm_teams.append(possible_amm_teams[i])
+        elif selection == '2':
+            chosen_amm_teams.append(possible_amm_teams[i+1])
+
+print('*****************************')
+print(chosen_wyn_teams)
+print('*****************************')
+print(chosen_amm_teams)
+
+            
 #next step: present each with two teams, and save the choice to teams list, and the matchup to opponentsDict
-                 
-wynTeams = []
-ammTeams = []
-
