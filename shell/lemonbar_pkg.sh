34df2af9de51b2f66ae3aab0eebde9704f7e60b8
WnndGws <WnndGws@users.noreply.github.com>
2021-05-18 09:44:53 +0800

    Only display if there are any

--- shell/lemonbar_pkg.sh
+++ shell/lemonbar_pkg.sh
@@ -3,22 +3,23 @@
 
 # Need to update or it wont know there are new packages
 
-# TODO; test if works with empty list
 ignore_list="(^$(rg "^Ignore.*" /etc/pacman.conf | sed -e 's/^.*= //' -e 's/ /$)|(^/g')$)"
 
 while true; do
     sudo pikaur -Sy > /dev/null 2>&1
 
     pac=$(pacman -Qqu | rg --invert-match --count "$ignore_list")
-    [ -z $pac ] && pac=0
+    if [ -n $pac ]; then
 
-    aur=$(pikaur -Qqu 2> /dev/null | rg --invert-match --count "$ignore_list")
-    # Need to minus 1 since pikaur outputs a non-blank blank line
-    #aur=$(awk -v a="$aur" 'BEGIN { printf a-1 }')
-    aur=$(awk -v a="$aur" -v p="$pac" 'BEGIN { printf a-p }')
+        aur=$(pikaur -Qqu 2> /dev/null | rg --invert-match --count "$ignore_list")
+        # Need to minus 1 since pikaur outputs a non-blank blank line
+        #aur=$(awk -v a="$aur" 'BEGIN { printf a-1 }')
+        aur=$(awk -v a="$aur" -v p="$pac" 'BEGIN { printf a-p }')
 
-    [ "$pac" = "0" ] && [ "$aur" = "0" ] && leader="L" || leader="H"
-    echo "P$leader$pac  $aur"
+        #[ "$pac" = "0" ] && [ "$aur" = "0" ] && leader="L" || leader="H"
+        echo "PH$pac  $aur"
+
+    fi
 
     sleep 3600
 done
