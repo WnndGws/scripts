1ab6696c245b3a46a3238f3e4604f5d8850faab7
WnndGws <WnndGws@users.noreply.github.com>
2021-05-22 06:38:51 +0800

    This file to test if my newsboat urls are alive

---
+++ python/urltest.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Takes url list and sees if theyre alive, used to test my newsboat urls
+"""
+
+#standard imports
+import requests
+
+with open("urldata", "r") as f:
+    data = f.read().split("\n")
+
+#Remove trailing ,
+data = data[:-1]
+
+for url in data:
+    try:
+        requests.get(url, headers={"User-Agent": "Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:86.0) Gecko/20100101 Firefox/86.0"}, timeout=10)
+    except:
+        print(f"{url} failed")
