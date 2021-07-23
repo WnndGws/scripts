5f1e1f4cfbe309a9ea89e7a7744b076712cf688a
WnndGws <WnndGws@users.noreply.github.com>
2021-07-24 05:45:08 +0800

    Think this works for ignored packages

--- shell/lemonbar_pkg.sh
+++ shell/lemonbar_pkg.sh
@@ -4,20 +4,20 @@
 
 # Need to update or it wont know there are new packages
 
-#ignore_list="(^$(rg "^Ignore.*" /etc/pacman.conf | sed -e 's/^.*= //' -e 's/ /$)|(^/g')$)"
+ignore_list="(^$(rg "^Ignore.*" /etc/pacman.conf | sed -e 's/^.*= //' -e 's/ /$)|(^/g')$)"
 
 while true; do
     sudo pikaur -Sy > /dev/null 2>&1
 
-    #pac=$(pacman -Qqu | rg --invert-match --count "$ignore_list")
-    pac=$(pacman -Qqu)
+    pac=$(pacman -Qqu | rg --invert-match --count "$ignore_list")
+    #pac=$(pacman -Qqu)
     if [ -n "$pac" ]; then
 
         pac=$(echo $pac | wc -l)
-        aur=$(pikaur -Qqu | wc -l)
-        #aur=$(pikaur -Qqu 2> /dev/null | rg --invert-match --count "$ignore_list")
+        #aur=$(pikaur -Qqu | wc -l)
+        aur=$(pikaur -Qqu 2> /dev/null | rg --invert-match --count "$ignore_list")
         # Need to minus 1 since pikaur outputs a non-blank blank line
-        #aur=$(awk -v a="$aur" 'BEGIN { printf a-1 }')
+        aur=$(awk -v a="$aur" 'BEGIN { printf a-1 }')
         aur=$(awk -v a="$aur" -v p="$pac" 'BEGIN { printf a-p }')
 
         #[ "$pac" = "0" ] && [ "$aur" = "0" ] && leader="L" || leader="H"
