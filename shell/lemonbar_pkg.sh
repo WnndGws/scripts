6d3b8237c4d246d2bf9853c23cdc99041c2e34c3
WnndGws <WnndGws@users.noreply.github.com>
2021-07-29 22:29:38 +0800

    Who knows anymore

--- shell/lemonbar_pkg.sh
+++ shell/lemonbar_pkg.sh
@@ -17,7 +17,7 @@ while true; do
         aur=$(pikaur -Qqu | wc -l)
         #aur=$(pikaur -Qqu 2> /dev/null | rg --invert-match --count "$ignore_list")
         # Need to minus 1 since pikaur outputs a non-blank blank line
-        aur=$(awk -v a="$aur" 'BEGIN { printf a-1 }')
+        #aur=$(awk -v a="$aur" 'BEGIN { printf a-1 }')
         aur=$(awk -v a="$aur" -v p="$pac" 'BEGIN { printf a-p }')
 
         #[ "$pac" = "0" ] && [ "$aur" = "0" ] && leader="L" || leader="H"
