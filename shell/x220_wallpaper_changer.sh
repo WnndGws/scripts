c28330899cad0469c05bf93b482b16ded78d1837
WnndGws <WnndGws@users.noreply.github.com>
2021-08-17 03:17:00 +0800

    Dont hardcode

--- shell/x220_wallpaper_changer.sh
+++ shell/x220_wallpaper_changer.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 ## Changes my wallpaper
 
-if [ "$(bspc query -M | wc -l)" -eq 2 ]; then
-  "$HOME/git/agendrum/agendrum.py" --resolution 1366x768 && xwallpaper --output LVDS1 --center /tmp/wallpaper_1366x768.png
-  "$HOME/git/agendrum/agendrum.py" --resolution 1920x1080 && xwallpaper --output HDMI3 --center /tmp/wallpaper_1920x1080.png
-else
-  "$HOME/git/agendrum/agendrum.py" --resolution 1366x768 && xwallpaper --center /tmp/wallpaper_1366x768.png
-fi
+# Set wallpaper
+xrandr -q | rg ' connected' | while read -r line; do
+  disp=$(echo "$line" | awk '{print $1}')
+  resol=$(echo "$line" | rg --only-matching '\d{4}x\d{3,4}')
+  "$HOME/git/agendrum/agendrum.py" --resolution "$resol" && xwallpaper --output "$disp" --center "/tmp/wallpaper_$resol.png"
+done
