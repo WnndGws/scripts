3d3e0c7b0480ee24d15dafda2bbf529354bb07f4
WnndGws <WnndGws@users.noreply.github.com>
2022-05-06 20:27:48 +0800

    feat(Add-timezone-checks-for-lemonbar): so i know what time it is in the world

---
+++ python/lemonbar_other_timezones.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Just prints the current time in London and Melbourne
+"""
+
+import datetime
+
+import pytz
+
+tz_melb = pytz.timezone("Australia/Melbourne")
+tz_london = pytz.timezone("Europe/London")
+print(
+    f'D{datetime.datetime.now(tz=tz_melb).strftime("%H")}h  {datetime.datetime.now(tz=tz_london).strftime("%H")}h'
+)
---
+++ shell/lemonbar_gettimez.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+##
+
+while true; do
+    "$HOME/git/scripts/python/lemonbar_other_timezones.py"
+    sleep 120
+done
